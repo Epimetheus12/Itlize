edb0ff1a7d3fec4c036caa35e369e79f9f7dd8b1
Epimetheus12 <liangfangji@126.com>
2021-11-16 17:25:32 -0500

    update User's part file

--- src/main/java/com/itlize/backend/demo/repositories/UserRepository.java
+++ src/main/java/com/itlize/backend/demo/repositories/UserRepository.java
@@ -1,4 +1,15 @@
 package com.itlize.backend.demo.repositories;
 
-public interface UserRepository {
+import com.itlize.backend.demo.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Repository
+@Transactional
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+    Optional <User> findByUsername (String username);
 }
